e9dbde721b74be1316cdbbefaf93b13d20e9b5e8
huangyeqin <970259402@qq.com>
2018-04-26 17:28:58 +0800

    文字识别
    图像文字识别

---
+++ testSSM/src/com/testSSM/test/controller/PhotoRenderController.java
@@ -0,0 +1,14 @@
+/**
+ * @author huangyq
+ * @date 2018-4-26  
+ * @version 1.0.0 
+ */
+package com.testSSM.test.controller;
+
+/**
+ * @author hyqin
+ *
+ */
+public class PhotoRenderController extends BaseController{
+	
+}
